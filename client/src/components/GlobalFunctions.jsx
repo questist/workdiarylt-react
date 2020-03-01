e6631c1c600e8aeb561554109d58533a507ccbbb
Rotimi <48189827+questist@users.noreply.github.com>
2020-02-29 19:45:13 -0800

    added graphqlyoga

--- client/src/components/GlobalFunctions.jsx
+++ client/src/components/GlobalFunctions.jsx
@@ -1,8 +1,14 @@
 //import React,{createContext} from 'react'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 export default function Globals() {
     console.log("Work Diary LT, Ver .1 Beta")
 }
 
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "blank"
+});
 
 let StatusEnum = {
     RUNNING: 1,
